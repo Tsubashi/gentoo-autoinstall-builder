c39f33058ecbe4ddacea3ad00ef91bcebe32b06b
Chandler Scott <cscott@aquaveo.com>
2017-07-18 09:06:50 -0600

    fix arguments in disk_prep

--- builder/disk_prep.sh
+++ builder/disk_prep.sh
@@ -3,14 +3,17 @@
 cd `dirname "${0}"`
 source builder.cfg
 
+echo "Rewriting disk label (GPT)"
 parted -s "${DEV}" mklabel gpt
-# GRUB Partition
-parted -s "${DEV}" mkpart primary 2048s 1M
-# Boot Partition
-parted -s "${DEV}" mkpart primary 1M 1G
-# Root Partition
+echo "Creating GRUB Partition"
+parted -s "${DEV}" mkpart primary 1M 2M
+parted -s "${DEV}" set 1 bios_grub on 
+echo "Creating Boot Partition"
+parted -s "${DEV}" mkpart primary 2M 1G
+echo "Creating Root Partition"
 parted -s "${DEV}" mkpart primary 1G 100%
 partprobe > /dev/null 2>&1
 
+echo "Installing filesystems"
 mkfs.ext4 -FF "${PART}"
 mkfs.ext2 -FF "${BOOT_PART}"
