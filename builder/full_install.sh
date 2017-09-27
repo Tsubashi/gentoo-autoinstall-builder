114d751301e780c953826d4c4bb3f47736362837
Chandler Scott <cscott@aquaveo.com>
2017-09-27 12:24:36 -0600

    configure system to use all cores

--- builder/full_install.sh
+++ builder/full_install.sh
@@ -36,6 +36,7 @@ echo "Extracting portage"
 ./extract_portage.sh
 cp -f package.use ${R}/etc/portage/package.use/all
 cp -f package.accept_keywords ${R}/etc/portage/package.accept_keywords
+echo "MAKEOPTS=\"$(nproc)\"" >> ${R}/etc/portage/make.conf
 cp -f /etc/resolv.conf ${R}/etc
 
 echo "Syncing portage (Just in Case)"
