59420f0f3b6be495bfa709b1672dea218830daa5
Chandler Scott <cscott@aquaveo.com>
2017-09-28 11:48:06 -0600

    add missing colon

--- builder/full_install.sh
+++ builder/full_install.sh
@@ -65,7 +65,7 @@ chroot_exec "cd /etc/init.d/; ln -sf net.lo net.eth0"
 
 # set up salt configuration
 cp -f salt-config ${R}/etc/salt/minion
-echo "id \"$(shuf -n1 adjectives.txt)-$(shuf -n1 first-names.txt)\"" >> ${R}/etc/salt/minion
+echo "id: \"$(shuf -n1 adjectives.txt)-$(shuf -n1 first-names.txt)\"" >> ${R}/etc/salt/minion
 
 # enable default services
 for service in acpid syslog-ng cronie net.eth0 sshd ntpd qemu-guest-agent salt-minion; do
